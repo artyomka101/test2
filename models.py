b5f581de8a7cf07e7c3b1b4998164d1f827d48c9
artyomka <artyomka1350@gmail.com>
2026-01-28 00:25:06 +0300

    Готовая практика

---
+++ models.py
@@ -0,0 +1,29 @@
+"""
+Модели данных для системы учёта учебных достижений
+"""
+
+class User:
+    def __init__(self, id=None, username=None, password=None, full_name=None, group_name=None):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.full_name = full_name
+        self.group_name = group_name
+
+class Subject:
+    def __init__(self, id=None, name=None, credits=None, semester=None, teacher=None):
+        self.id = id
+        self.name = name
+        self.credits = credits
+        self.semester = semester
+        self.teacher = teacher
+
+class Grade:
+    def __init__(self, id=None, user_id=None, subject_id=None, grade=None, 
+                 grade_type=None, date_received=None):
+        self.id = id
+        self.user_id = user_id
+        self.subject_id = subject_id
+        self.grade = grade
+        self.grade_type = grade_type
+        self.date_received = date_received
